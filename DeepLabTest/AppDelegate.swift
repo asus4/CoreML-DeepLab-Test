ba8becf6eaeb1bf779299c7eb8388cc70830bcb2
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-11-05 17:07:54 +0100

    Initial commot

---
+++ DeepLabTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DeepLabTest
+//
+//  Created by Koki Ibukuro on 2019/11/05.
+//  Copyright © 2019 Koki Ibukuro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DeepLabTest/ViewController.swift
@@ -0,0 +1,269 @@
+//
+//  ViewController.swift
+//  DeepLabTest
+//
+//  Created by Koki Ibukuro on 2019/11/05.
+//  Copyright © 2019 Koki Ibukuro. All rights reserved.
+//
+
+import Cocoa
+import CoreML
+import Vision
+import CoreGraphics
+
+//https://github.com/tensorflow/models/blob/master/research/deeplab/utils/get_dataset_colormap.py
+
+let ColorTable:[[UInt8]] = [
+    [0, 0, 0],
+    [120, 120, 120],
+    [180, 120, 120],
+    [6, 230, 230],
+    [80, 50, 50],
+    [4, 200, 3],
+    [120, 120, 80],
+    [140, 140, 140],
+    [204, 5, 255],
+    [230, 230, 230],
+    [4, 250, 7],
+    [224, 5, 255],
+    [235, 255, 7],
+    [150, 5, 61],
+    [120, 120, 70],
+    [8, 255, 51],
+    [255, 6, 82],
+    [143, 255, 140],
+    [204, 255, 4],
+    [255, 51, 7],
+    [204, 70, 3],
+    [0, 102, 200],
+    [61, 230, 250],
+    [255, 6, 51],
+    [11, 102, 255],
+    [255, 7, 71],
+    [255, 9, 224],
+    [9, 7, 230],
+    [220, 220, 220],
+    [255, 9, 92],
+    [112, 9, 255],
+    [8, 255, 214],
+    [7, 255, 224],
+    [255, 184, 6],
+    [10, 255, 71],
+    [255, 41, 10],
+    [7, 255, 255],
+    [224, 255, 8],
+    [102, 8, 255],
+    [255, 61, 6],
+    [255, 194, 7],
+    [255, 122, 8],
+    [0, 255, 20],
+    [255, 8, 41],
+    [255, 5, 153],
+    [6, 51, 255],
+    [235, 12, 255],
+    [160, 150, 20],
+    [0, 163, 255],
+    [140, 140, 140],
+    [250, 10, 15],
+    [20, 255, 0],
+    [31, 255, 0],
+    [255, 31, 0],
+    [255, 224, 0],
+    [153, 255, 0],
+    [0, 0, 255],
+    [255, 71, 0],
+    [0, 235, 255],
+    [0, 173, 255],
+    [31, 0, 255],
+    [11, 200, 200],
+    [255, 82, 0],
+    [0, 255, 245],
+    [0, 61, 255],
+    [0, 255, 112],
+    [0, 255, 133],
+    [255, 0, 0],
+    [255, 163, 0],
+    [255, 102, 0],
+    [194, 255, 0],
+    [0, 143, 255],
+    [51, 255, 0],
+    [0, 82, 255],
+    [0, 255, 41],
+    [0, 255, 173],
+    [10, 0, 255],
+    [173, 255, 0],
+    [0, 255, 153],
+    [255, 92, 0],
+    [255, 0, 255],
+    [255, 0, 245],
+    [255, 0, 102],
+    [255, 173, 0],
+    [255, 0, 20],
+    [255, 184, 184],
+    [0, 31, 255],
+    [0, 255, 61],
+    [0, 71, 255],
+    [255, 0, 204],
+    [0, 255, 194],
+    [0, 255, 82],
+    [0, 10, 255],
+    [0, 112, 255],
+    [51, 0, 255],
+    [0, 194, 255],
+    [0, 122, 255],
+    [0, 255, 163],
+    [255, 153, 0],
+    [0, 255, 10],
+    [255, 112, 0],
+    [143, 255, 0],
+    [82, 0, 255],
+    [163, 255, 0],
+    [255, 235, 0],
+    [8, 184, 170],
+    [133, 0, 255],
+    [0, 255, 92],
+    [184, 0, 255],
+    [255, 0, 31],
+    [0, 184, 255],
+    [0, 214, 255],
+    [255, 0, 112],
+    [92, 255, 0],
+    [0, 224, 255],
+    [112, 224, 255],
+    [70, 184, 160],
+    [163, 0, 255],
+    [153, 0, 255],
+    [71, 255, 0],
+    [255, 0, 163],
+    [255, 204, 0],
+    [255, 0, 143],
+    [0, 255, 235],
+    [133, 255, 0],
+    [255, 0, 235],
+    [245, 0, 255],
+    [255, 0, 122],
+    [255, 245, 0],
+    [10, 190, 212],
+    [214, 255, 0],
+    [0, 204, 255],
+    [20, 0, 255],
+    [255, 255, 0],
+    [0, 153, 255],
+    [0, 41, 255],
+    [0, 255, 204],
+    [41, 0, 255],
+    [41, 255, 0],
+    [173, 0, 255],
+    [0, 245, 255],
+    [71, 0, 255],
+    [122, 0, 255],
+    [0, 255, 184],
+    [0, 92, 255],
+    [184, 255, 0],
+    [0, 133, 255],
+    [255, 214, 0],
+    [25, 194, 194],
+    [102, 255, 0],
+    [92, 0, 255],
+]
+
+class ViewController: NSViewController {
+    
+    @IBOutlet weak var resultImageView: NSImageView!
+    
+    var model: VNCoreMLModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let model = (try? VNCoreMLModel(for: DeepLabV3().model)) else {
+            // Could not load MLModel
+            // Check "FCRN.mlmodel" exists in your project
+            fatalError()
+        }
+        self.model = model
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+    
+    func processImage(_ url: URL) {
+        print("process image", url)
+        
+        
+        let request = VNCoreMLRequest(model: self.model, completionHandler: onVisionRequestComplete)
+        request.imageCropAndScaleOption = .centerCrop
+        
+        let handler = VNImageRequestHandler(url: url, options: [:])
+        try? handler.perform([request])
+        
+        
+//        self.resultImageView.image = NSImage(contentsOf: url)
+    }
+
+    @IBAction func loadButtonClick(_ sender: Any) {
+        
+        let panel = NSOpenPanel()
+        panel.canChooseFiles = true
+        panel.canChooseDirectories = false
+        panel.allowsMultipleSelection = false
+        panel.allowedFileTypes = ["jpg", "png"]
+        
+        panel.begin { (result) in
+            if result.rawValue != NSApplication.ModalResponse.OK.rawValue {
+                return
+            }
+            guard let url = panel.url else {
+                return
+            }
+            self.processImage(url)
+            
+        }
+    }
+    
+    private func onVisionRequestComplete(request: VNRequest, error: Error?) {
+        
+        guard let observations = request.results as? [VNCoreMLFeatureValueObservation],
+              let depth = observations.first?.featureValue.multiArrayValue else {
+            return
+        }
+        
+        // 
+        let width = 513
+        let height = 513
+        var data: [UInt8] = [UInt8](repeating: 255, count: width * height * 4)
+        for y in 0 ..< height {
+            for x in 0 ..< width {
+                let i = y * width + x
+                let table = ColorTable[depth[i].intValue]
+                data[i * 4 + 0] = table[0]
+                data[i * 4 + 1] = table[1]
+                data[i * 4 + 2] = table[2]
+                data[i * 4 + 3] = 255
+            }
+        }
+        
+        data.withUnsafeBytes { ptr in
+            let context = CGContext(data: UnsafeMutableRawPointer(mutating: ptr.baseAddress!),
+                                    width: width,
+                                    height: height,
+                                    bitsPerComponent: 8,
+                                    bytesPerRow: width * 4,
+                                    space: CGColorSpaceCreateDeviceRGB(),
+                                    bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue)
+            guard let image = context?.makeImage() else  {
+                print("no image")
+                return
+            }
+            self.resultImageView.image = NSImage(cgImage: image, size: NSSize(width: width, height: height))
+        }
+        
+    }
+    
+    
+    
+}
+
